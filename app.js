b0845fe2ac1c6f503d345e7739417d0898481e2a
c-usher <usher.cody@outlook.com>
2021-06-06 22:41:27 -0600

    message is able to be sent with app.use and res.json

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use('/', (req, res, next) => {
+    res.json({message: "Message works"});
+})
+
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
 });
