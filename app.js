03ad3c80f43b4b623fee32a63afb37e55518e3e6
c-usher <usher.cody@outlook.com>
2021-06-07 20:54:15 -0600

    cors, helmet, morgan, bodyparser are set up

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use('/', (req, res, next) => {
-    res.json({message: "Message works"});
-})
+//API security
+app.use(helmet());
+
+//Handle cors error
+app.use(cors());
+
+//Logs each request
+app.use(morgan("tiny"));
+
+//Set body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", (req, res) => {
+  res.json({ message: "Message works!!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
