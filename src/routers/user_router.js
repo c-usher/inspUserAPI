e460336a29cf9bf986275d6d3090732d2794f185
c-usher <usher.cody@outlook.com>
2021-06-24 20:24:00 -0600

    bcrypt is working

--- src/routers/user_router.js
+++ src/routers/user_router.js
@@ -1,25 +1,34 @@
 const express = require("express");
 const { route } = require("./unit_router");
-const router = express.Router()
-const {insertUser} = require('../model/User_model')
+const router = express.Router();
+const { insertUser } = require("../model/User_model");
+const { hashPassword } = require("../helpers/bcrypt_helper");
 
 router.all("/", (req, res, next) => {
-    
-    // res.json({ message: "this message is from user router" })
+  // res.json({ message: "this message is from user router" })
 
-    next();
+  next();
+});
 
-})
+router.post("/", async (req, res) => {
+  const { name, phone, email, password } = req.body;
+  
+  try {
+    //HashPass
+      const hashedPass = await hashPassword(password);
+      const newUserObj = {
+        name,
+        phone,
+        email,
+        password: hashedPass,
+      };
 
-router.post('/', async (req, res) => {
-    try {
-            const result = await insertUser(req.body);
-            console.log(result);
-            res.json({ message: "new user created!", result });
-
-    } catch (error) {
-        console.log(error);
-        res.json({ status: 'error', message: error.message});
-    }
-})
+    const result = await insertUser(newUserObj);
+    console.log(result);
+    res.json({ message: "new user created!", result });
+  } catch (error) {
+    console.log(error);
+    res.json({ status: "error", message: error.message });
+  }
+});
 module.exports = router;
