0a1e99aa227967f9ff05b822866bb8bc9a4fabee
c-usher <usher.cody@outlook.com>
2021-08-20 20:45:22 -0600

    working on heroku error

--- app.js
+++ app.js
@@ -32,12 +32,12 @@ mDb.on("error", (error) => {
 //Logs each request
 app.use(morgan("tiny"));
 
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   //set static folder
+//   app.use(express.static("client/build"));
+// }
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "app.js"));
+  res.sendFile(path.resolve(__dirname));
 });
 
 // if (process.env.NODE_ENV !== "production") {
