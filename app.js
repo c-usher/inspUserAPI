9fa9ec34c06ed009a050977f2ed97f441a0b0a90
c-usher <usher.cody@outlook.com>
2021-08-20 20:46:52 -0600

    moved app.get *

--- app.js
+++ app.js
@@ -36,9 +36,6 @@ app.use(morgan("tiny"));
 //   //set static folder
 //   app.use(express.static("client/build"));
 // }
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname));
-});
 
 // if (process.env.NODE_ENV !== "production") {
 //   const mDb = mongoose.connection;
@@ -86,6 +83,9 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname));
+});
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
 });
